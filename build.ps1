695f400feddcd10f18541922df9f9aa1425ba4dd
Simon Duff <simon.j.duff@gmail.com>
2018-07-15 18:10:20 +0100

    Yaml generation

--- build.ps1
+++ build.ps1
@@ -1,5 +1,12 @@
+$ErrorActionPreference = "Stop"
+
+yaml2json -s "m-chee-daro.yml"
+
 cd app
 npm install
 npm run build
 Remove-Item -Recurse -Path ../docs -Exclude "_config.yml" -Force
+
+Copy-Item -Force "../m-chee-daro.json" build
+
 Copy-Item -Force -Recurse "build/*" "../docs/"
