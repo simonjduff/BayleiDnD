6d7e9242446944559bc6be34a1638c5cf72e40ce
Simon Duff <simon.j.duff@gmail.com>
2018-07-15 14:38:50 +0100

    Components

---
+++ app/src/components/character.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Character extends React.Component{
+    render(){
+        return (
+        <div className='character-box dndbox'>
+            Yes
+        </div>
+        );
+    }
+}
+
+export default Character;
