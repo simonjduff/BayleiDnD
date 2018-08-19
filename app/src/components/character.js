96af8775cc8274a411acdb87ef47e32baa3103b5
Simon Duff <simon.j.duff@gmail.com>
2018-08-19 13:02:24 +0100

    Print formatting

--- app/src/components/character.js
+++ app/src/components/character.js
@@ -6,38 +6,38 @@ export default class Character extends React.Component{
     render(){
         return (
             <Grid container>
-                <Grid item lg={3}>
+                <Grid item xs={3} lg={3}>
                     <Paper className='character-name dndbox'>
                         <div className='character-value'>{this.props.Character.Name}</div>
                         <div className='character-label'>Character</div>
                     </Paper>
                 </Grid>
-                <Grid item lg={9}>
+                <Grid item xs={9} lg={9}>
                     <Paper className='character-box dndbox'>
                         <Grid container>
-                            <Grid item className='character-class character-container' lg={4}>
+                            <Grid item className='character-class character-container' xs={4} lg={4}>
                                 <div className='character-value'>{this.props.Character.Class}</div>
                                 <div className='character-label'>Class</div>
                             </Grid>
-                            <Grid item lg={4}>
+                            <Grid item xs={4} lg={4}>
                                 <div className='character-value'>{this.props.Character.Background.Name}</div>
                                 <div className='character-label'>Background</div>
                             </Grid>
-                            <Grid item lg={4}>
+                            <Grid item xs={4} lg={4}>
                                 <div className='character-value'>{this.props.Character.PlayerName}</div>
                                 <div className='character-label'>Player</div>
                             </Grid>
                         </Grid>
                         <Grid container>
-                            <Grid item lg={4}>
+                            <Grid item xs={4} lg={4}>
                                 <div className='character-value'>{this.props.Character.Race}</div>
                                 <div className='character-label'>Race</div>
                             </Grid>
-                            <Grid item lg={4}>
+                            <Grid item xs={4} lg={4}>
                                 <div className='character-value'>{this.props.Character.Alignment}</div>
                                 <div className='character-label'>Alignment</div>
                             </Grid>
-                            <Grid item lg={4}>
+                            <Grid item xs={4} lg={4}>
                                 <div className='character-value'>N/A</div>
                                 <div className='character-label'>XP</div>
                             </Grid>
